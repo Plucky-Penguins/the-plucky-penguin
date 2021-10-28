1f6e804b2ca872ef24bde8d7d3b1377d7c7fc58e
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2021-10-28 08:30:57 -0700

    updated collectible system to only add one per fish, updated sprites

--- Assets/Scripts/PlayerCollect.cs
+++ Assets/Scripts/PlayerCollect.cs
@@ -19,7 +19,7 @@ public class PlayerCollect : MonoBehaviour
 
     public void ChangeScore(int coinValue) {
         score += coinValue;
-        collection.text = "" + score.ToString() + "/560";
+        collection.text = "" + score.ToString() + "/66";
     }
 
 }
