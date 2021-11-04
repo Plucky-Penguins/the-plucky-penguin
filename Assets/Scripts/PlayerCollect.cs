ad4afab55402723abb855174053a4cbe9ffc0757
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2021-11-04 03:13:39 -0700

    updated fish and player collection script

--- Assets/Scripts/PlayerCollect.cs
+++ Assets/Scripts/PlayerCollect.cs
@@ -7,19 +7,20 @@ public class PlayerCollect : MonoBehaviour
 {
     public TextMeshProUGUI collection;
     int level_score;
+
     // total_score should be carried across levels
     int total_score;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("collectible")) {
-            int coinValue = other.gameObject.GetComponent<Fish>().coinValue;
-            ChangeScore(coinValue);
+            int fish_value = other.gameObject.GetComponent<Fish>().fish_value;
+            collectFish(fish_value);
             Destroy(other.gameObject);
         }
 
     }
 
-    public void ChangeScore(int coinValue) {
+    public void collectFish(int coinValue) {
         level_score += coinValue;
         /** TODO: /66 needs to be turned into a variable 
          * changed per level
