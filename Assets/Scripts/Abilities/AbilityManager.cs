bf293935fd471d54b14d6c883233ed5956cbcc84
Kotailri <willc8810@gmail.com>
2021-11-04 23:00:37 -0700

    added cooldown display to ui

--- Assets/Scripts/Abilities/AbilityManager.cs
+++ Assets/Scripts/Abilities/AbilityManager.cs
@@ -7,33 +7,42 @@ using UnityEngine;
 /// </summary>
 public class AbilityManager : MonoBehaviour
 {
-    public AbilityInterface.IAbility abilitySlot1 = null;
-    public AbilityInterface.IAbility abilitySlot2 = null;
-    public AbilityInterface.IAbility abilitySlot3 = null;
+    public List<AbilityInterface.IAbility> abilities = new List<AbilityInterface.IAbility>();
+
+    public Dictionary<int, string> slotToKey = new Dictionary<int, string>();
 
     private void Start()
     {
+        slotToKey.Add(0, "E");
+        slotToKey.Add(1, "R");
+        slotToKey.Add(2, "F");
+
+        for(int i = 0; i < 3; i++)
+        {
+            abilities.Add(null);
+        }
+            
         // example of how to add abilities
-        abilitySlot1 = GetComponent<BombAbility>();
-        //abilitySlot2 = GetComponent<ProjectileAbility>();
-        //abilitySlot3 = GetComponent<ShieldAbility>();
+        abilities[0] = GetComponent<BombAbility>();
+        abilities[1] = GetComponent<ProjectileAbility>();
+        abilities[2] = GetComponent<BurstAbility>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && abilitySlot1 != null) // activate first ability slot
+        if (Input.GetKeyDown(KeyCode.E) && abilities[0] != null) // activate first ability slot
         {
-            abilitySlot1.activateAbility();
+            abilities[0].activateAbility();
         }
         
-        if (Input.GetKeyDown(KeyCode.R) && abilitySlot2 != null) // activate second ability slot
+        if (Input.GetKeyDown(KeyCode.R) && abilities[1] != null) // activate second ability slot
         {
-            abilitySlot2.activateAbility();
+            abilities[1].activateAbility();
         }
         
-        if (Input.GetKeyDown(KeyCode.F) && abilitySlot3 != null) // activate third ability slot
+        if (Input.GetKeyDown(KeyCode.F) && abilities[2] != null) // activate third ability slot
         {
-            abilitySlot3.activateAbility();
+            abilities[2].activateAbility();
         }
 
 
